846370db3696d3944cdec4f30dd5da049b2ff2fb
kostik-loyko <kostik.loyko@mail.ru>
2022-05-10 00:30:32 +0300

    feat: add lin for github

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ keyboard.classList.add('keyboard');
 const keys = document.createElement('ul');
 keys.classList.add('keys');
 
+const link = document.createElement('a');
+link.classList.add('link');
+link.setAttribute('href', 'https://github.com/kostik-loyko/virtual-keyboard');
+link.textContent = 'Ссылка на гитхаб';
+
+const p = document.createElement('p');
+p.classList.add('p');
+p.textContent = 'Переключение языка Control + Alt';
+
 const keyEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', 'Space', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
 const keyEnBig = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Delete', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
 const keyRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
@@ -21,6 +30,8 @@ const codes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'D
 
 body.appendChild(textarea);
 body.appendChild(keyboard);
+body.appendChild(link);
+body.appendChild(p);
 keyboard.appendChild(keys);
 
 // функция создающая раскладку клавиатуры + регистр, аргумен - массив с соответствующим языком ↓
